8f7fbff69308f1fc96a956c94610dd2a908e9a21
lucasjia <lucasjia@ucsb.edu>
2024-09-07 20:56:08 -0700

    react + tailwind + django initial set up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
